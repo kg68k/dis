860c820ac90d86a37df012e20ace6cfc973ae07c
kg68k <78926718+kg68k@users.noreply.github.com>
2024-12-06 02:19:11 +0900

    build: add Package.mk

---
+++ src/Package.mk
@@ -0,0 +1,58 @@
+# Makefile for dis (create archive file)
+#  usage: mkdir pkgtmp; make -C pkgtmp -f ../Package.mk
+
+SRCDIR_MK = ../../srcdir.mk
+SRC_DIR = ../../src
+-include $(SRCDIR_MK)
+
+ROOT_DIR = $(SRC_DIR)/..
+BUILD_DIR = ..
+
+CP_P = cp -p
+U8TOSJ = u8tosj
+
+DIS_ZIP = $(BUILD_DIR)/dis.zip
+
+DIRS = docs include
+
+DOCS_ = analysis.txt CHANGELOG.txt gpl-3.0.txt labelfile.txt README.txt table.txt tablefile.txt
+DOCS = $(addprefix docs/,$(DOCS_))
+
+INCLUDES_ = doscall.mac fefunc.mac iocscall.mac sxcall.mac
+INCLUDES = $(addprefix include/,$(INCLUDES_))
+
+PROGRAM = dis.x
+
+FILES = $(DOCS) $(INCLUDES) $(PROGRAM)
+
+.PHONY: all
+
+all: $(DIS_ZIP)
+
+$(DIRS):
+	mkdir $@
+
+docs/CHANGELOG.txt: $(ROOT_DIR)/CHANGELOG.md
+	$(U8TOSJ) < $^ >! $@
+
+docs/gpl-3.0.txt: $(ROOT_DIR)/docs/gpl-3.0.txt
+	rm -f $@
+	$(CP_P) $^ $@
+
+docs/%: $(ROOT_DIR)/docs/%
+	$(U8TOSJ) < $^ >! $@
+
+include/%: $(ROOT_DIR)/include/%
+	$(U8TOSJ) < $^ >! $@
+
+$(PROGRAM): $(BUILD_DIR)/$(PROGRAM)
+	rm -f $@
+	$(CP_P) $^ $@
+	strip $@
+
+$(DIS_ZIP): $(DIRS) $(FILES)
+	rm -f $@
+	zip -9 $@ $^
+
+
+# EOF
